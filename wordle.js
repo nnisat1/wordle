12f3ed2d1bb5d947ebda961c6e151f004756df95
nazifa nisat <18nisatna@gmail.com>
2025-10-29 14:10:03 +0000

    new

---
+++ wordle.js
@@ -0,0 +1,18 @@
+var height = 5;
+var width = 6;
+
+var row=0;
+var col=0;
+
+var gameOver= false;
+var word = "SQUID";
+
+const letters = document.querySelector(".letter")
+console.log (letters)
+//console.log (letters.item(0))
+ //letters.item(0).addEventListener("change", inputTextEvent)
+ 
+
+function inputTextEvent (e) {
+    console.log(e)
+}

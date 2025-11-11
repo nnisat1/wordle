dd079557ec30f805d74064ea0ec90f80286a155c
Nazifa Nisat <nazifa.nisat@uk.ey.com>
2025-11-11 09:20:59 +0000

    fixed row issues

--- wordle.js
+++ wordle.js
@@ -7,12 +7,39 @@ var col=0;
 var gameOver= false;
 var word = "SQUID";
 
-const letters = document.querySelector(".letter")
-console.log (letters)
-//console.log (letters.item(0))
- //letters.item(0).addEventListener("change", inputTextEvent)
- 
+const letters = document.querySelectorAll(".scoreboard-tile")
+let currentLetterIndex = 0
 
-function inputTextEvent (e) {
-    console.log(e)
+const wordUrl = "https://words.dev-apis.com/word-of-the-day"
+
+async function getWordOfTheDay() {
+    const word = await fetch(wordUrl)
+    console.log( await word.json())
 }
+
+getWordOfTheDay()
+
+document.addEventListener ("keydown",handleKeyDown)
+
+function handleKeyDown(event) {
+   
+    if (event.key == "Backspace") {
+        if (currentLetterIndex>0 && letters.item(currentLetterIndex).innerText == ""){
+            currentLetterIndex --
+        }
+        letters.item(currentLetterIndex).innerText = ""
+        
+    } else if ( currentLetterIndex==4 && letters.item(currentLetterIndex).innerText != "") {
+        return
+    } else {
+        letters.item(currentLetterIndex).innerText = event.key
+        if (currentLetterIndex <4 ){
+            currentLetterIndex ++
+        }
+    }
+}
+
+
+
+
+
